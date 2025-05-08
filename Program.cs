c6fd977f81adab66da1a55a98a2ea0094cc95115
Bernardo Robaina <bernardo.robaina@serel.com.br>
2025-05-08 20:34:57 -0300

    Loop logic through table's names

--- Program.cs
+++ Program.cs
@@ -23,6 +23,23 @@ namespace Basedian
                     Console.WriteLine("State: {0}", connection.State);
                     Console.WriteLine("ConnectionString: {0}", connection.ConnectionString);
 
+                    var transaction = connection.BeginTransaction();
+
+                    SqlCommand command = new SqlCommand("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'", connection);
+
+                    command.Transaction = transaction;  
+
+                    // Loops each table in database 
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(String.Format("{0}",
+                                reader[0]));
+                            Thread.Sleep(20);
+                        }
+                    }
+
                 }
             }
             catch (Exception ex)
